9838373a31efac103aa6f575eec1227429b9a9a6
Fahmi_Alamsyah_N <alamsyahfahmi99@gmail.com>
2023-02-21 06:38:22 +0700

    p

--- bag_cepattanggap/visi_misi.php
+++ bag_cepattanggap/visi_misi.php
@@ -90,7 +90,8 @@ include 'partindex/script.php'
       </nav>
     </section>
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</html>
 
     <div class="move_date" style=" font-size: x-large; font-family: arial;">
       <p class="inside_date">
@@ -101,5 +102,5 @@ include 'partindex/script.php'
     </div>
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
 </html>
