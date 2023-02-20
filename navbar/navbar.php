b8d25f07e0207c3cc60938ea3c1d1910c8557aa5
Fahmi_Alamsyah_N <alamsyahfahmi99@gmail.com>
2023-02-20 19:00:00 +0700

    p

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -8,7 +8,7 @@ include '../script/script.php'
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PT CTM</title>
-    <link rel="stylesheet" href="CSS/index.css" />
+    <link rel="stylesheet" href="css/index.css" />
     <link rel="shortcut icon" href="../img/logo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,12 +16,33 @@ include '../script/script.php'
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Hepta+Slab&family=Kdam+Thmor+Pro&family=PT+Sans&family=Silkscreen&family=Ubuntu+Mono&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
 
+<style>
+  section{
+    background-color: #800000;
+    font-family:Arial;
+  }
+
+  .nav-item:hover{
+    background-color: red;
+  }
+
+  .nav-item dropdown{
+    color: aliceblue;
+    font-size: large;
+  }
+  
+  .nav-link{
+    color: aliceblue;
+    font-size: large;
+  }
+</style>
+
 </head>
 <body>
     <section>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand ms-5 fs-3" href="index.php"> <img src="../img/logo.png" alt=""></a>
+          <a class="navbar-brand ms-5 fs-3" href="#"> <img src="../img/logo.png" alt=""></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -63,7 +84,7 @@ include '../script/script.php'
         </div>
       </nav>
     </section>
-    <br>
+    <hr size="10">
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </html>
