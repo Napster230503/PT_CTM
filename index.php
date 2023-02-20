4a9bdb3ad3a3305e22c869f05e08b88045f82abd
Fahmi_Alamsyah_N <alamsyahfahmi99@gmail.com>
2023-02-20 21:38:14 +0700

    p

--- index.php
+++ index.php
@@ -1,26 +1,6 @@
 <?
 include 'partindex/script.php'
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PT CTM</title>
-    <link rel="stylesheet" href="css/index.css" />
-    <link rel="shortcut icon" href="img/logo.png">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Hepta+Slab&family=Kdam+Thmor+Pro&family=PT+Sans&family=Silkscreen&family=Ubuntu+Mono&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
-
-<body>
-<?
-include '../script/script.php'
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <meta charset="UTF-8">
@@ -38,6 +18,8 @@ include '../script/script.php'
     <script src="js/jquery.js" type="text/javascript"></script>
     <script src="js/bootstrap.js" type="text/javascript"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
+    <body>
 <style>
   section{
     background-color: #800000;
