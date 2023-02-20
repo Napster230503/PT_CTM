361327e541b5e577d76112faf541e36efe45a8b6
Fahmi_Alamsyah_N <alamsyahfahmi99@gmail.com>
2023-02-20 20:13:20 +0700

    p

--- partindex/navbar.php
+++ partindex/navbar.php
@@ -84,7 +84,6 @@ include '../script/script.php'
         </div>
       </nav>
     </section>
-    <hr size="10">
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </html>
